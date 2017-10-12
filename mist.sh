1b4e45f29e739d09bb5ddfd0a23b861fc6207161
Hirano Takanori <hirano.takanori@consensus-base.com>
2017-10-12 21:07:19 +0900

    no message

--- mist.sh
+++ mist.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#screen -S mist /Applications/Mist.app/Contents/MacOS/Mist --rpc ./ethereum/geth.ipc
-screen -S mist /Applications/Mist.app/Contents/MacOS/Mist --rpc http://localhost:8545 --swarmurl "null"
+screen -S mist /Applications/Mist.app/Contents/MacOS/Mist --rpc ./ethereum/geth.ipc
+#screen -S mist /Applications/Mist.app/Contents/MacOS/Mist --rpc http://localhost:8545 --swarmurl "null"
