8691c79db9a7e20bb6300a356f1770764d0ce32f
Takanori Hirano <hrn.takanori@gmail.com>
2018-02-16 14:24:25 +0900

    :wrench:

--- geth/start.sh
+++ geth/start.sh
@@ -8,9 +8,11 @@ screen -S geth \
     --rpc \
     --rpcapi "eth,net,web3,personal,admin" \
     --rpccorsdomain '*' \
+    --rpcaddr [::] \
     --ws \
     --wsapi "eth,net,web3,personal,admin" \
     --wsorigins "*" \
+    --wsaddr [::] \
     --unlock "0xbfe06b543964758e1e79033798e684371b7f9996" \
     --password /dev/null \
     --mine \
